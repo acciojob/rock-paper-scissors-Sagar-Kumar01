04fe5091e49af3358a416a2b292672865f674c24
sagar kumar <94098920+Sagar-Kumar01@users.noreply.github.com>
2025-10-29 13:42:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 //your code here
-document.getElementById("demo")
+let game_number = document.getElementById("game-number");
+
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissor = document.getElementById("scissor");
+
+let play_games = document.getElementById("play-games");
+
+let round_result = document.getElementById("round-result");
+let computer_choose = document.getElementById("computer-choose");
+let rounds_left = document.getElementById("rounds-left");
+
+let user_points = document.getElementById("user-points");
+let computer_points = document.getElementById("computer-points");
+let game_result = document.getElementById("game-result");
+
+let isPlayTap = null;
+
 
 window.computer = {
-  choice: Math.floor(Math.random() * 3)
+  choice: [0,1,2]
 };
-const options = ["ROCK", "PAPER", "SCISSORS"];
-let computer = options[window.computer.choice];
+
+function computerBot() {
+	const options = ["ROCK", "PAPER", "SCISSORS"];
+    return options[window.computer.choice[Math.floor(Math.random() * 3)]];
+	
+}
+
+play_games.addEventListener("click",()=>{
+	isPlayTap = 1;
+	rounds_left.textContent = game_number.value;
+});
+
 
 
 
@@ -14,3 +41,7 @@ let computer = options[window.computer.choice];
 
 // let turnLeft = ;
 // let roundresult = ;
+
+
+
+
