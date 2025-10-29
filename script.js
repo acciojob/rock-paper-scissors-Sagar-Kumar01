96e0f90b30533028e8902ab41f5ba457a617abbd
sagar kumar <94098920+Sagar-Kumar01@users.noreply.github.com>
2025-10-29 13:03:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+document.getElementById("demo")
+
+window.computer = {
+  choice: Math.floor(Math.random() * 3)
+};
+const options = ["ROCK", "PAPER", "SCISSORS"];
+let computer = options[window.computer.choice];
+
+
+
+// let userPoint = 0;
+// let computerPoint = 0;
+
+// let turnLeft = ;
+// let roundresult = ;
